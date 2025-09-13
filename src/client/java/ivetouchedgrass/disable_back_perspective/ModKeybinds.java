48ecd229a128eb660c970703e97010e4bd0f357a
IveTouchedGrass <109032090+IveTouchedGrass@users.noreply.github.com>
2025-09-13 10:59:41 +0300

    Improved compatibility with other mods

--- src/client/java/ivetouchedgrass/disable_back_perspective/ModKeybinds.java
+++ src/client/java/ivetouchedgrass/disable_back_perspective/ModKeybinds.java
@@ -1,8 +1,10 @@
 package ivetouchedgrass.disable_back_perspective;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class ModKeybinds {
@@ -13,6 +15,18 @@ public class ModKeybinds {
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_BACKSLASH,
                 "key.categories.gameplay"
-        ));
+        ) {
+            private boolean oldPressed = false;
+
+            @Override
+            public void setPressed(boolean pressed) {
+                super.setPressed(pressed);
+                if (pressed && !oldPressed && MinecraftClient.getInstance().player != null) {
+                    DisableFrontPerspective.isModEnabled = !DisableFrontPerspective.isModEnabled;
+                    MinecraftClient.getInstance().player.sendMessage(DisableFrontPerspective.isModEnabled ? Text.translatable("disable_back_perspective.enabled_mod").withColor(0x00FF00) : Text.translatable("disable_back_perspective.disabled_mod").withColor(0xFF0000), true);
+                }
+                oldPressed = pressed;
+            }
+        });
     }
 }
