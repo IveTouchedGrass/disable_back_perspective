b986588dea143e489b39f87778cda85111398d41
IveTouchedGrass <109032090+IveTouchedGrass@users.noreply.github.com>
2026-05-07 23:06:23 +0300

    Ported the mod to 26.1.x

--- src/client/java/ivetouchedgrass/disable_back_perspective/ModKeybinds.java
+++ src/client/java/ivetouchedgrass/disable_back_perspective/ModKeybinds.java
@@ -1,29 +1,29 @@
 package ivetouchedgrass.disable_back_perspective;
 
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 
 public class ModKeybinds {
-    public static KeyBinding TOGGLE_MOD;
+    public static KeyMapping TOGGLE_MOD;
     public static void registerKeybinds() {
-        TOGGLE_MOD = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        TOGGLE_MOD = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 "key.disable_back_perspective.toggle",
-                InputUtil.Type.KEYSYM,
+                InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_BACKSLASH,
-                KeyBinding.Category.MISC
+                KeyMapping.Category.MISC
         ) {
             private boolean oldPressed = false;
 
             @Override
-            public void setPressed(boolean pressed) {
-                super.setPressed(pressed);
-                if (pressed && !oldPressed && MinecraftClient.getInstance().player != null) {
+            public void setDown(boolean pressed) {
+                super.setDown(pressed);
+                if (pressed && !oldPressed && Minecraft.getInstance().player != null) {
                     DisableFrontPerspective.isModEnabled = !DisableFrontPerspective.isModEnabled;
-                    MinecraftClient.getInstance().player.sendMessage(DisableFrontPerspective.isModEnabled ? Text.translatable("disable_back_perspective.enabled_mod").withColor(0x00FF00) : Text.translatable("disable_back_perspective.disabled_mod").withColor(0xFF0000), true);
+                    Minecraft.getInstance().player.sendOverlayMessage(DisableFrontPerspective.isModEnabled ? Component.translatable("disable_back_perspective.enabled_mod").withColor(0x00FF00) : Component.translatable("disable_back_perspective.disabled_mod").withColor(0xFF0000));
                 }
                 oldPressed = pressed;
             }
--- src/client/java/ivetouchedgrass/disable_back_perspective/mixin/MixinMinecraft.java
+++ src/client/java/ivetouchedgrass/disable_back_perspective/mixin/MixinMinecraft.java
@@ -2,18 +2,18 @@ package ivetouchedgrass.disable_back_perspective.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import ivetouchedgrass.disable_back_perspective.DisableFrontPerspective;
-import net.minecraft.client.option.Perspective;
+import net.minecraft.client.CameraType;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(Perspective.class)
+@Mixin(CameraType.class)
 public abstract class MixinMinecraft {
-    @ModifyReturnValue(method = "next", at = @At("RETURN"))
-    private Perspective next(Perspective original) {
+    @ModifyReturnValue(method = "cycle", at = @At("RETURN"))
+    private CameraType next(CameraType original) {
         if (DisableFrontPerspective.isModEnabled) {
-            return switch ((Perspective) (Object) this) {
-                case FIRST_PERSON, THIRD_PERSON_BACK -> Perspective.THIRD_PERSON_FRONT;
-                case THIRD_PERSON_FRONT -> Perspective.FIRST_PERSON;
+            return switch ((CameraType) (Object) this) {
+                case FIRST_PERSON, THIRD_PERSON_BACK -> CameraType.THIRD_PERSON_FRONT;
+                case THIRD_PERSON_FRONT -> CameraType.FIRST_PERSON;
             };
         }
         return original;
